99d7c5ba9f032eaf11203cf359123c58df9e14d4
Anatolijs <vpl@inbox.lv>
2018-05-08 12:15:54 +0300

    Adding 2 controllers.

--- src/main/java/mycompany/api/image/api/controller/ImageController.java
+++ src/main/java/mycompany/api/image/api/controller/ImageController.java
@@ -1,18 +1,49 @@
-package mycompany.api.image.api.endpoint;
+package mycompany.api.image.api.controller;
 
+import mycompany.api.image.api.model.Image;
+import mycompany.api.image.api.service.ImageService;
 import org.springframework.stereotype.Component;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.Collection;
 
 @Component
-@Path("/api")
-public class Endpoint {
+@Path("/images")
+public class ImageController {
+
+    private ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @GET
-    @Path("/ping")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response.ok("pong").build();
+    @Path("application/json")
+    public Collection getAllImages() {
+        return imageService.getAllImages();
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/{id}")
+    public Image getImage(@PathParam("id") String id) {
+        return imageService.getImage(id);
+    }
+
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response addImage(Image image) {
+        imageService.addImage(image);
+        return Response.created(URI.create("/" + image.getId())).build();
+    }
+
+    @PUT
+    @Consumes("application/json")
+    @Path("/{id}")
+    public Response updateImage(@PathParam("id") String id, Image image) {
+        imageService.updateImage(id, image);
+        return Response.noContent().build();
+    }
 }

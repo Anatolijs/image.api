9047b768d7c7c08105d33bffd3c87efe6780ca3f
Anatolijs <vpl@inbox.lv>
2018-05-08 12:18:28 +0300

    Jersey configuration.

--- src/main/java/mycompany/api/image/api/JerseyConfig.java
+++ src/main/java/mycompany/api/image/api/JerseyConfig.java
@@ -1,4 +1,20 @@
 package mycompany.api.image.api;
 
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import static org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.register;
+
+@Configuration
+@ApplicationPath("rest")
 public class JerseyConfig {
+    public JerseyConfig() {
+
+    }
+
+    @PostConstruct
+    public void setUp() {
+        register();
+        register();
+    }
 }

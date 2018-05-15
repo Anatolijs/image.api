044b53212327cbe374939e011609dc1bc0cbfb31
Anatolijs <vpl@inbox.lv>
2018-05-15 10:33:26 +0300

    Map changing to ConcurrentMap.

--- src/main/java/mycompany/api/image/api/service/ImageService.java
+++ src/main/java/mycompany/api/image/api/service/ImageService.java
@@ -3,14 +3,16 @@ package mycompany.api.image.api.service;
 import mycompany.api.image.api.model.Image;
 import org.springframework.stereotype.Service;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class ImageService {
 
-    private final Map<String, Image> images;
+    private final ConcurrentMap<Integer, Image> images;
 
     public ImageService() {
-        this.images = new HashMap<>();
+        this.images = new ConcurrentHashMap<>();
     }
 
     public Collection<Image> getAllImages() {
@@ -18,16 +20,17 @@ public class ImageService {
         return allImages.isEmpty() ? Collections.emptyList(): new ArrayList<>(allImages);
     }
 
-    public Image getImage(String id) {
+    public Image getImage(Integer id) {
         return images.get(id);
     }
+
     public void addImage(Image image) {
-        if (Objects.isNull(image.getId())) image.setId(UUID.randomUUID().toString());
+        if (Objects.isNull(image.getId())) image.setId(Integer.valueOf(UUID.randomUUID().toString()));
 
         this.images.put(image.getId(), image);
     }
 
-    public Image updateImage(String id, Image image) {
+    public Image updateImage(Integer id, Image image) {
         if (images.containsKey(id)) {
             return images.put(id, image);
         }

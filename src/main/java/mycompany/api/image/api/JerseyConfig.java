7712d820abd5b739dd26ac5be00446f03e7a3a48
Anatolijs <vpl@inbox.lv>
2018-05-24 10:58:03 +0300

    @Bean removed.

--- src/main/java/mycompany/api/image/api/JerseyConfig.java
+++ src/main/java/mycompany/api/image/api/JerseyConfig.java
@@ -1,13 +1,8 @@
 package mycompany.api.image.api;
 
 import mycompany.api.image.api.controllers.ImageController;
-import mycompany.api.image.api.service.ImageService;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
 import javax.ws.rs.ApplicationPath;
 
 
@@ -18,15 +13,6 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         packages(getClass().getPackage().getName() + ".resources");
         register(ImageController.class);
-        register(ImageService.class);
+        register(CorsResponseFilter.class);
     }
-
-    @Bean
-    public ViewResolver viewResolver() {
-
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/");
-        resolver.setSuffix(".json");
-        return resolver;
-   }
 }

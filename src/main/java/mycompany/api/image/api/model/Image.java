5b48cb99dd4c9669f8cc93c54e5194cfe8b1de3a
Anatolijs <vpl@inbox.lv>
2018-05-24 09:21:56 +0300

    Changes in @Entity construction.

--- src/main/java/mycompany/api/image/api/model/Image.java
+++ src/main/java/mycompany/api/image/api/model/Image.java
@@ -1,23 +1,36 @@
 package mycompany.api.image.api.model;
 
 import javax.persistence.*;
+import java.util.List;
+
 
 @Entity
-@Table(name = "images")
+@Table(name= "images")
 public class Image {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
     private String name;
-    private String id;
 
-    public Image() {}
+    @Column(name="tags")
+    @ElementCollection
+    private List<String> tags;
 
-    public Image(String name, String id) {
-        super();
-        this.name = name;
-        this.id = id;
+    public Image() {
     }
+
+    public Image(String name, List<String> tags) {
+        this.name = name;
+        this.tags = tags;
+    }
+
+    public List<String> getTags() {return tags;}
+
+    public void setTags(List<String> tags) {this.tags = tags;}
 
     public String getName() {
         return name;
@@ -27,17 +40,17 @@ public class Image {
         this.name = name;
     }
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
     @Override
     public String toString() {
 
-        return "Image{" + "id=" + id + ", name= '" + name + '}';
+        return "Image{" + "id= " + id + ", name= " + name + "}";
     }
 }
